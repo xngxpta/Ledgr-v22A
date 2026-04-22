96ea4ff82a906081056ed6053af5d146194c4d71
r <146114263+xngxpta@users.noreply.github.com>
2026-04-22 23:16:09 +0530

    Update Stripe URL in Home.py

--- Home.py
+++ Home.py
@@ -38,7 +38,7 @@ url_fbook = "https://www.facebook.com/share/1BnXaYvRzV/"
 url_insta = 'https://www.instagram.com/alphaledgr/'
 url_blog = 'https://www.alphaledgr.com/Blog'
 url_linkedin = "https://www.linkedin.com/company/ledgrapp/"
-url_stripe = "https://book.stripe.com/9B6bJ3gWS87G97b80q0480f"
+url_stripe = "https://book.stripe.com/cNi6oJ3625Zy97b94u0480g"
 url_stripe_2 = "https://buy.stripe.com/6oUbJ35eaew4bfj0xY0480e"
 st.sidebar.link_button("Access Pro / Day!", url_stripe, type="primary",
                         disabled=False, use_container_width=True)
