38f35aacd92c9c50575a3251c0051d59d9917ad2
r <146114263+xngxpta@users.noreply.github.com>
2026-06-08 19:03:11 +0530

    Add borders to the form in AnalyticsBox.py

--- pages/3 - AnalyticsBox.py
+++ pages/3 - AnalyticsBox.py
@@ -69,7 +69,7 @@ with mx2:
 # #################################
 
 # Form and Inputs ############################################################
-with st.form():
+with st.form(borders=True):
     stock = st.selectbox("Please select stock ticker", tickerlist)
     submitted = st.form_submit_button("Proceed")
     if not submitted:
