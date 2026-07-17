ef3d25852d65474949ad578317242e8bd61eb1a2
r <146114263+xngxpta@users.noreply.github.com>
2026-07-17 22:43:27 +0530

    Fix Google Tag Manager script formatting

--- pages/1 - LedgrBase.py
+++ pages/1 - LedgrBase.py
@@ -25,7 +25,7 @@ import streamlit.components.v1 as components
 
 GA_ID = "G-4MRDCEFGB4"
 
-gtag_html = f("""
+gtag_html = """
 <script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -33,7 +33,7 @@ gtag_html = f("""
   gtag('js', new Date());
   gtag('config', '{GA_ID}');
 </script>
-""")
+"""
 # Embed the script
 components.html(gtag_html, height=0)
 
